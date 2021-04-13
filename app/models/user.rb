b0c44151151ca6a1d3e656ad9c04969a277ad509
chibi0310mayu <ilove0910bio@gmail.com>
2021-04-13 12:46:41 +0900

    ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :fist_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :fist_name_kana, presence: true
+  validates :birthdate, presence: true
+
+
+
+
+  
 end
