44e7b410eeb320598d507dcd6b364395e1530d25
chibi0310mayu <ilove0910bio@gmail.com>
2021-04-14 16:11:53 +0900

    ActiveStorageを導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :items
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
 
   with_options presence: true do
      validates :nickname
