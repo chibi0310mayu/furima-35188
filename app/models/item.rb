307b5c142c68a060a9a19ce95524d12d14797264
chibi0310mayu <ilove0910bio@gmail.com>
2021-04-14 18:02:23 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :shipping
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :price
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :condition_id
+    validates :burden_id
+    validates :area_id
+    validates :shipping_id
+  end
 end
