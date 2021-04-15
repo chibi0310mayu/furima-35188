35ae68e0a125947b2e3d54c46ca7281e71bf76a7
chibi0310mayu <ilove0910bio@gmail.com>
2021-04-15 11:06:05 +0900

    追加バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,8 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :explanation
-    validates :price
+    validates :price,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },format: { with: /^[0-9]+$/ }
+
     validates :image
   end
 
