bf00d97401c30bd09695d686cc86bbb25eb39bd3
chibi0310mayu <ilove0910bio@gmail.com>
2021-04-19 12:03:46 +0900

    修正しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_session, only: [:edit, :update]
+  before_action :move_to_session, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
