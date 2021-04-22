793496c0dccbc7a5e43a2d57f3ff3f7f3c69b28f
chibi0310mayu <ilove0910bio@gmail.com>
2021-04-22 18:05:39 +0900

    rubokop実行

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -2,11 +2,10 @@ FactoryBot.define do
   factory :purchase_address do
     post_code { '123-4567' }
     prefecture_id { 2 }
-    city { '東京都 '}
-    home_num { ' 1-1 '}
+    city { '東京都 ' }
+    home_num { ' 1-1 ' }
     building_name { '東京ハイツ' }
-    tel { '09012345678'}
-    token {"tok_abcdefghijk00000000000000000"}
-    
+    tel { '09012345678' }
+    token { 'tok_abcdefghijk00000000000000000' }
   end
 end
